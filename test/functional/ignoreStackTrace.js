66d1d475ed2b10202543198e04a7b81da52c08f9
Jamie Sherriff <sherriffjamie@gmail.com>
2017-11-12 11:58:58 +1300

    Add in chai for test assertions start work on more reporter options

---
+++ test/functional/ignoreStackTrace.js
@@ -0,0 +1,78 @@
+'use strict';
+const {execFile} = require('child_process');
+const chai = require('chai');
+const assert = chai.assert;
+const expect = chai.expect;
+const { logMochaOutput, getMochaPath } = require('./helpers');
+const internalMochaPath = getMochaPath();
+
+describe('Check TeamCity Output is correct with outer suite', function () {
+	let teamCityStdout, teamCityStderr, teamCityOutputArray;
+	function verifyResults() {
+		it('stdout output should exist', function () {
+			assert.ok(teamCityStdout, 'has output');
+			assert.ok(teamCityOutputArray, 'array of output is populated');
+			assert.ok(teamCityOutputArray.length >= 10, 'at least 10 lines of output');
+		});
+
+		it('stderr output should not exist', function () {
+			assert.ok(teamCityStderr.length === 0);
+		});
+
+		it('custom stacktrace is present', function () {
+			const rowToCheck = teamCityOutputArray[4];
+			expect(rowToCheck).to.include('##teamcity[testFailed');
+			expect(rowToCheck).to.include('name=\'Failing Test @fail');
+			expect(rowToCheck).to.include('message=\'custom stacktrace replacement');
+			expect(rowToCheck).to.include('flowId=');
+			expect(rowToCheck).to.not.include('duration=');
+			expect(rowToCheck).to.include('details=\'2 == 1');
+			expect(rowToCheck).to.include('captureStandardOutput=\'true\'');
+			expect(rowToCheck).to.not.include('simple.js:11:11');
+			expect(rowToCheck).to.include(']');
+		});
+	}
+
+	describe('specified as an env var', function () {
+		before(function (done) {
+			const opts = {
+				env: Object.assign({
+					MOCHA_TEAMCITY_STACK_TRACE_MESSAGE: 'custom stacktrace replacement'
+				}, process.env)
+			};
+
+			execFile(internalMochaPath, [
+				'test/test_data',
+				'--reporter',
+				'lib/teamcity'
+			], opts, (err, stdout, stderr) => {
+				teamCityStdout = stdout;
+				teamCityStderr = stderr;
+				teamCityOutputArray = stdout.split('\n');
+				logMochaOutput(stdout, stderr);
+				done();
+			});
+		});
+		verifyResults();
+	});
+
+	describe('specified with --reporter-options', function () {
+		before(function (done) {
+			execFile(internalMochaPath, [
+				'test/test_data',
+				'--reporter',
+				'lib/teamcity',
+				'--reporter-options',
+				'ignoreStackTrace=custom stacktrace replacement'
+			], (err, stdout, stderr) => {
+				teamCityStdout = stdout;
+				teamCityStderr = stderr;
+				teamCityOutputArray = stdout.split('\n');
+				logMochaOutput(stdout, stderr);
+				done();
+			});
+		});
+		verifyResults();
+	});
+
+});
